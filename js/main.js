07baeca8bd2c6d8f2ec6b5be46c4ca1e08ca54df
Ali Mohamed <alicastilo1097@gmail.com>
2023-05-06 23:02:41 +0300

    add animation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// AOS.init();
+// AOS.init({
+//     offset: 200,
+//     delay: 100,
+//     duration: 400,
+//     easing: 'ease',
+//     once: false,
+//     mirror: false,
+//     anchorPlacement: 'top-bottom'
+// });
+
+
